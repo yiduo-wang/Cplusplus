8bdaaf328bb6c533f3337a67b7fc7edd269cafa9
yiduo-wang <1163268812@qq.com>
2019-08-09 14:14:25 +0800

    类模板博客代码

--- 类模板课堂代码/Project1/源.cpp
+++ 类模板课堂代码/Project1/源.cpp
@@ -16,10 +16,98 @@ typedef int DataType;
 //	size_t _capacity;
 //};
 
-//C++泛型
-template<class T>
-//类型:Sqlist<T>
-//类名:Seqlist
+////C++泛型
+//template<class T>
+////类型:Sqlist<T>
+////类名:Seqlist
+//class Seqlist
+//{
+//public:
+//	Seqlist()//构造函数
+//	{
+//		_arr = new T[10];
+//		_size = 0;
+//		_capacity = 0;
+//	}
+//	~Seqlist();//析构函数
+//	Seqlist(const Seqlist<T>& s);//拷贝构造
+//	Seqlist<T>& operator=(const Seqlist<T>& s);//赋值运算符重载
+//	size_t Size()
+//	{
+//		return _size;
+//	}
+//
+//	void PushBack(const T& x)//模板尽量传引用和用const
+//	{
+//		//if (size >= _capacity)
+//		//{
+//		//	//增容
+//		//}
+//		_arr[_size] = x;
+//		++_size;
+//	}
+//	void Popback()
+//	{
+//		if (_size > 0)
+//		{
+//			--_size;
+//		}
+//	}
+//	T operator[](size_t index)
+//	{
+//		return _arr[index];
+//	}
+//	void Print()
+//	{
+//		for (size_t i=0;i<_size;++i)
+//		{
+//			cout << _arr[i] << " ";
+//		}
+//		cout << endl;
+//	}
+//private:
+//	T* _arr;
+//	size_t _size;
+//	size_t _capacity;
+//};
+//
+////类外面定义成员函数,定义和声明分离
+//template<class T>
+//Seqlist<T>::~Seqlist()
+//{
+//	delete[] _arr;
+//}
+//
+//
+//struct Node
+//{};
+//
+//int main()
+//{
+//	Seqlist <int>s1;//int
+//	//Seqlist <Node*>s2;//Node*
+//	//s1,s2不是同一个类的对象,但是是同一个类模板的
+//	s1.PushBack(1);
+//	s1.PushBack(2);
+//	s1.PushBack(3);
+//	//s1.Print();
+//	for (size_t i = 0; i < s1.Size(); ++i)
+//	{
+//		cout << s1[i] << " ";
+//		cout << s1.operator[](i) << " ";
+//	}
+//	cout << endl;
+//	system("pause");
+//}
+//template<class T>
+//void Swap(T& left,T& right)
+//{
+//	T tmp = left;
+//	left = right
+//	right = tmp;
+//}
+
+template <class T>
 class Seqlist
 {
 public:
@@ -29,40 +117,37 @@ public:
 		_size = 0;
 		_capacity = 0;
 	}
-	~Seqlist();//析构函数
-	Seqlist(const Seqlist<T>& s);//拷贝构造
-	Seqlist<T>& operator=(const Seqlist<T>& s);//赋值运算符重载
-	size_t Size()
+	~Seqlist()//析构函数
 	{
-		return _size;
+		delete[] _arr;
 	}
-
-	void PushBack(const T& x)//模板尽量传引用和用const
+	Seqlist(const Seqlist<T>& data)//拷贝构造
 	{
-		//if (size >= _capacity)
-		//{
-		//	//增容
-		//}
-		_arr[_size] = x;
-		++_size;
+		_arr = data._arr;
+		_size = data._size;
+		_capacity = data._capacity;
 	}
-	void Popback()
+	Seqlist<T>& operator=(const Seqlist<T>& data)//赋值运算符重载
+	{
+		_arr = data._arr;
+		_size = data.size;
+		_capacity = data._capacity;
+	}
+	void PushBack(const T& data)//尾插
+	{
+		//不考虑增容问题
+		_arr[_size] = data;
+		_size++;
+	}
+	void PopBack()//尾删
 	{
 		if (_size > 0)
-		{
 			--_size;
-		}
 	}
-	T operator[](size_t index)
+	void Print()//打印
 	{
-		return _arr[index];
-	}
-	void Print()
-	{
-		for (size_t i=0;i<_size;++i)
-		{
-			cout << _arr[i] << " ";
-		}
+		for (size_t i = 0; i < _size; ++i)
+			cout << _arr[i]<<" ";
 		cout << endl;
 	}
 private:
@@ -71,38 +156,17 @@ private:
 	size_t _capacity;
 };
 
-//类外面定义成员函数,定义和声明分离
-template<class T>
-Seqlist<T>::~Seqlist()
-{
-	delete[] _arr;
-}
-
-
-struct Node
-{};
-
 int main()
 {
-	Seqlist <int>s1;//int
-	//Seqlist <Node*>s2;//Node*
-	//s1,s2不是同一个类的对象,但是是同一个类模板的
+	Seqlist<int> s1;
+	Seqlist<double>s2;
 	s1.PushBack(1);
 	s1.PushBack(2);
 	s1.PushBack(3);
-	//s1.Print();
-	for (size_t i = 0; i < s1.Size(); ++i)
-	{
-		cout << s1[i] << " ";
-		cout << s1.operator[](i) << " ";
-	}
-	cout << endl;
+	s1.Print();
+	s2.PushBack(1.1);
+	s2.PushBack(2.2);
+	s2.PushBack(3.3);
+	s2.Print();
 	system("pause");
 }
-//template<class T>
-//void Swap(T& left,T& right)
-//{
-//	T tmp = left;
-//	left = right
-//	right = tmp;
-//}
